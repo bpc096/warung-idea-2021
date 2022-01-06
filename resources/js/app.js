f7bbb1107290c51ef2ac288cbf512aea691b68a8
mfaisalghozi <faisal.ghozi@dana.id>
2022-01-07 01:00:30 +0700

    fix: bug vue types umd

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,12 +12,10 @@ import Axios from 'axios'
 import Vue from 'vue'
 import router from './router'
 import store from './store'
-
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { setHeaderToken } from './utils/auth'
 
-import { setHeaderToken } from '../utils/auth'
-import { axios } from 'vue/types/umd';
 
 Axios.defaults.baseURL = 'http://localhost:8000/api/'
 
@@ -25,15 +23,17 @@ Vue.config.productionTip = false
 Vue.use(Axios)
 
 const token = localStorage.getItem('token')
-
 if(token) {
   setHeaderToken(token)
 }
 
-
 store.dispatch('get_user', token)
 .then( () => {
-  new Vue(Vue.util.extend({ router, store }, App)).$mount("#app");
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
 })
 .catch(err => {
   console.error(err)
