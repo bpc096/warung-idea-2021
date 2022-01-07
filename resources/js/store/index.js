bc09e307da50a52dcc51d970404783373f9e8a42
mfaisalghozi <faisal.ghozi@dana.id>
2022-01-07 10:32:43 +0700

    feat: creating modules store auth js

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,91 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
 import {
   ADDING_NUMBER,
   SUBTRACT_NUMBER,
-  SET_USER,
-  RESET_USER,
 } from './mutation-types'
+import authStore from './modules/authStore'
 
-const state = {
+const state__ = {
   number: 1,
-  user: null,
-  isLoggedIn: false,
 }
 
-const actions = {
+const actions__ = {
   addingNumber ({commit, state}) {
     commit(ADDING_NUMBER, 1)
   },
   subtractNumber({commit, state}) {
     commit(SUBTRACT_NUMBER, 1)
   },
-  login({ dispatch, commit }, data) {
-    return new Promise((resolve, reject) => {
-      axios.post('login-api', data)
-       .then(response => {
-         const token = response.data.token
-         localStorage.setItem('token', token)
-         setHeaderToken(token)
-         dispatch('get_user')
-         resolve(response)
-       })
-       .catch(err => {
-         commit(RESET_USER)
-         localStorage.removeItem('token')
-         reject(err)
-      })
-    })
-  },
-  async get_user({commit}){
-    if(!localStorage.getItem('token')){
-      return
-    }
-    try{
-      let response = await axios.get('user-api')
-        commit(SET_USER, response.data.data)
-    } catch (error){
-        commit(RESET_USER)
-        removeHeaderToken()
-        localStorage.removeItem('token')
-        return error
-    }
-  }
 }
 
-const mutations = {
+const mutations__ = {
   [ADDING_NUMBER] (state, data) {
     state.number = state.number + data
   },
   [SUBTRACT_NUMBER] (state, data) {
     state.number = state.number - data
   },
-  [SET_USER] (state, data) {
-    state.user = data
-    state.isLoggedIn = true
-  },
-  [RESET_USER] (state) {
-    state.user = null
-    state.isLoggedIn = false
-  }
 }
 
-const getters = {
-  isLoggedIn (state) {
-    return state.isLoggedIn
-  },
-  user (state) {
-    return state.user
-  }
+const getters__ = {
+
 }
 
 Vue.use(Vuex)
 const store = new Vuex.Store({
-  state,
-  actions,
-  mutations,
-  getters,
+  state: {
+    ...state__,
+    ...authStore.state,
+  },
+  actions: {
+    ...actions__,
+    ...authStore.actions,
+  },
+  mutations: {
+    ...mutations__,
+    ...authStore.mutations,
+  },
+  getters: {
+    ...getters__,
+    ...authStore.getters,
+  },
 })
 
 export default store
