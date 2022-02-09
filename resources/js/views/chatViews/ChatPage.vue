c596f0d07f86c92585902a1d0d2b04ec97adcaad
mfaisalghozi <mrafgaming@gmail.com>
2022-02-09 20:26:54 +0700

    feat: adding private chat

---
+++ resources/js/views/chatViews/ChatPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="chat-page-wrap">
+    Chat page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatPage',
+}
+</script>
+
+<style lang="less" scoped>
+.chat-page-wrap {
+  min-height: 90vh;
+}
+</style>
