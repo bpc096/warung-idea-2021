f3cc626c23100e21601c0c023026ae4651e5cba9
mfaisalghozi <mrafgaming@gmail.com>
2022-06-09 01:14:31 +0700

    feat: fixing category page

--- resources/js/views/CategoryPage.vue
+++ resources/js/views/CategoryPage.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="category-wrapper">
     <div class="title-category">
+      <div class="darker">
+        <img
+          :src="getCategoryBanner"
+          alt="title-banner"
+        >
+      </div>
       <h1>{{ queryCategoryId }}</h1>
       <p>{{ getCategoryDescription }}</p>
     </div>
@@ -37,6 +43,14 @@ export default {
         'A book is a medium for recording information in the form of writing or images, typically composed of many pages bound together and protected by a cover. The technical term for this physical arrangement is codex.',
         'A film – also called a movie, motion picture, moving picture, or photoplay – is a work of visual art that simulates experiences and otherwise communicates ideas, stories, perceptions, feelings, beauty, or atmosphere through the use of moving images.',
         'Regular exercise improves health and fitness. Health is defined as a state of complete mental, physical and social well-being; not merely the absence of illness or infirmity. Fitness is the ability to meet the demands of the environment.',
+      ],
+      categoryBanner: [
+        'https://images.unsplash.com/photo-1460661419201-fd4cecdf8a8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80',
+        'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2340&q=80',
+        'https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+        'https://images.unsplash.com/photo-1495446815901-a7297e633e8d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2340&q=80',
+        'https://images.unsplash.com/photo-1615986201152-7686a4867f30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1925&q=80',
+        'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
       ],
       isExist: true,
       projectList: 7,
@@ -78,6 +92,10 @@ export default {
     getCategoryDescription() {
       const categoryIndex = this.getCategoryId - 1
       return this.categoryDesc[categoryIndex]
+    },
+    getCategoryBanner() {
+      const categoryIndex = this.getCategoryId - 1
+      return this.categoryBanner[categoryIndex]
     }
   },
   methods: {
@@ -108,17 +126,42 @@ export default {
   .title-category {
     text-align: left;
     margin: 5rem 2rem;
+    min-height: 10rem;
+
+    .darker {
+      background-color: black;
+      height: 20rem;
+      width: 100%;
+      position: absolute;
+      left: 0;
+      top: 4rem;
+    }
+    img {
+      height: 20rem;
+      width: 100%;
+      background-size: auto;
+      background-repeat: no-repeat;
+      object-fit: cover;
+      position: absolute;
+      left: 0;
+      opacity: 0.5;
+    }
     h1 {
       font-size: 48px;
       font-weight: 700;
       letter-spacing: 2px;
-
+      position: relative;
+      z-index: 2;
+      color: #fff;
     }
     p {
       font-size: 18px;
       font-weight: 500;
       width: 70%;
       letter-spacing: 1px;
+      position: relative;
+      z-index: 2;
+      color: #fff;
     }
   }
 
